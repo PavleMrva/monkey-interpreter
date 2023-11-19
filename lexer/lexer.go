e6fd2b77a111e5a4ac7d2aac6befd3e74e079394
PavleMrva <pavlemrvaljevic1@gmail.com>
2023-11-19 20:45:35 +0100

    feat: parse identifiers and integer literals

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -38,6 +38,48 @@ func (l *Lexer) peekChar() byte {
 	}
 }
 
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch ...byte) token.Token {
+	literal := ""
+
+	for i := range ch {
+		literal += string(ch[i])
+	}
+
+	return token.Token{Type: tokenType, Literal: literal}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
@@ -107,46 +149,4 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.readChar()
 	return tok
-}
-
-func (l *Lexer) skipWhitespace() {
-	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
-		l.readChar()
-	}
-}
-
-func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
-	}
-
-	return l.input[position:l.position]
-}
-
-func isLetter(ch byte) bool {
-	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
-}
-
-func (l *Lexer) readNumber() string {
-	position := l.position
-	for isDigit(l.ch) {
-		l.readChar()
-	}
-
-	return l.input[position:l.position]
-}
-
-func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
-}
-
-func newToken(tokenType token.TokenType, ch ...byte) token.Token {
-	literal := ""
-
-	for i := range ch {
-		literal += string(ch[i])
-	}
-
-	return token.Token{Type: tokenType, Literal: literal}
 }
